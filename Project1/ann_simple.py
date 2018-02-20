9c3429f8b76ec57402503e1fb144c3c48e80d9c8
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-02-19 16:15:02 -0900

    Add sigmoid prime

--- Project1/ann_simple.py
+++ Project1/ann_simple.py
@@ -14,6 +14,7 @@ class ANN:
         self.S2 = None
         self.Z1 = None
         self.Z2 = None
+        self.SM = None
 
     def foward_learning(self, X):
         # input to Layer1
@@ -26,13 +27,28 @@ class ANN:
 
         print("Z2", self.Z2)
         print("Z1", self.Z1)
-        return self.softmax(self.Z2)
+        self.SM = self.softmax(self.Z2)
+        return self.SM
 
     def sigmoid(self, s):
         return 1 / (1 + np.exp(-s))  # 1 / (1 + e^-s)
+
+    # Derivitive of the sigmoid function
+    # sigmoid(s) = (u'v -uv') / v^2
+    #   where u = 1, v = 1 + e^(-s)
+    #   (0 * v - 1 * -e^(-s)) / (1 + e^-s)^2
+    def sigmoid_prime(self, z):
+        return np.exp(-z) / (1+np.exp(-z)**2)
 
     def softmax(self, z):
         soft = np.full((self.outputLayerSize, 1), 0.10)
         max_index = np.argmax(z)
         soft[max_index] = 0.9
         return soft
+
+    # Back popagating error (Delta)
+    # DELTA[n] = -(y-y_hat) * sigmoid_prime(self.Z)
+    ## delta[n] = (-Y - self.SM) *
+    # where n refers to the layer starting with 0 for input
+    def back_propagation(self, X, y):
+        delta2 = np.multiply(-(Y-self.SM))
