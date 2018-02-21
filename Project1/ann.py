5aa49215d1afe204bd69e693b3168ea21daa2e86
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-02-20 18:00:51 -0900

    Adding work from study group

--- Project1/ann.py
+++ Project1/ann.py
@@ -3,41 +3,59 @@ import numpy as np
 class ANN:
     def __init__(self):
         # Layer definitions
-        self.inputLayerSize = 16
-        self.hiddenLayerSize = 20
-        self.hiddenLayerWidth = 2
+        self.inputLayerSize = 17
+        self.hiddenLayerSize = 17
         self.outputLayerSize = 26
+        self.learn = 0.01
 
         # Layer arrays
-        self.W_in = np.random.random_integers(-1, 1, (self.inputLayerSize, self.hiddenLayerSize))
-        self.W = np.random.random_integers(-1, 1, (self.hiddenLayerSize, self.hiddenLayerWidth))
-        self.W_out = np.random.random_integers(-1, 1, (self.hiddenLayerSize, self.outputLayerSize))
-        self.S_in = None
-        self.S = None
-        self.S_out = None
+        self.W1 = np.random.random_integers(-1, 1, (self.inputLayerSize, self.hiddenLayerSize))
+        self.W2 = np.random.random_integers(-1, 1, (self.hiddenLayerSize, self.outputLayerSize))
+        self.S1 = None
+        self.S2 = None
+        self.Z1 = None
+        self.Z2 = None
+        self.SM = None
 
-    def foward_learning(self, X):
+    def foward(self, X):
         # input to Layer1
-        self.S_in = np.dot(X, self.W_in)
-        self.Z_in = self.sigmoid(self.S_in)
-
-        self.S = np.dot(self.Z_in, self.W[:, 0])
-
-        # Layer1 to Layer2 (N to N+!)
-
-        # LayerFinal to output
+        self.S1 = np.dot(X, self.W1)
+        self.Z1 = self.sigmoid(self.S1)
+        self.S2 = np.dot(self.Z1, self.W2)
+        self.Z2 = self.sigmoid(self.S2) #self.Z2 = yhat
+        self.SM = self.softmax(self.Z2)
+        print("Z1", self.Z1.shape)
+        print("W2", self.W2.shape)
+        print("S2", self.S2.shape)
+        return self.SM
 
     def sigmoid(self, s):
         return 1 / (1 + np.exp(-s))  # 1 / (1 + e^-s)
 
+    def sigmoid_prime(self, z):
+        return np.exp(-z) / (1+np.exp(-z)**2)
+
+    def softmax(self, z):
+        soft = np.full(self.outputLayerSize, 0.10)
+        max_index = np.argmax(z)
+        soft[max_index] = 0.9
+        return soft
+
+    # Back popagating error (Delta)
+    # DELTA[n] = -(y-y_hat) * sigmoid_prime(self.Z)
+    ## delta[n] = (-Y - self.SM) *
+    # where n refers to the layer starting with 0 for input
+    def back_propagation(self, X, Y):
+        # print(self.SM)
+        # print("SM:",self.SM.shape,"Y:", Y.shape, "S2:", self.S2.shape)
+        # print(np.subtract(Y,self.SM))
+        delta2 = np.multiply(-(np.subtract(Y,self.SM)), self.sigmoid_prime(self.S2))
+        print("D2",delta2)
+        djdW2 = np.dot(self.Z1.T, delta2)
+
+        delta = np.dot(delta2, self.W2.T)*self.sigmoid_prime(self.S1)
+        djdW1 = np.dot(X.T, delta)
+        self.W2 = self.W2 - self.learn*djdW2
+        self.W1 = self.W1 - self.learn*djdW1
 
 
-# X = np.random.randn(1, 16)
-# print(X)
-# [[ 0.5493172   0.3859487   0.09700825 -0.70588039 -1.10151935  0.19557332
-#    0.13441547 -0.10486223 -0.61647325  0.35607019  0.84703342  0.80315358
-#   -1.40575479 -1.41993189  1.14724125 -0.72441052]]
-# W = np.random.randn(16, 10)
-# np.dot(X, W)
-# array([[-2.67992058,  0.58073405,  0.63232844,  2.65453628, -2.0576537 ,
-#         -1.29921952,  3.69145145,  0.00949092,  5.81982076, -0.41185868]])
--- Project1/letter_ocr.py
+++ Project1/letter_ocr.py
@@ -1,19 +1,33 @@
 import numpy as np
 import pandas as pd
-import ann_simple
+import ann
+#np.set_printoptions(threshold=np.nan)
 
-data = pd.read_csv('./Letters.csv', header=None)
+data = pd.read_csv('./letters.csv', header=None)
 
 rows = data.shape[0]
 cols = data.shape[1]
 data = data.values
+numberWrong = 0
 
-X = data[:, (range(1, cols))]
-Y = data[:, 0] # Snag the first column corresponding to the letter
+X = np.array(data[:, (range(1, cols))], dtype = float)
+X = np.insert(X,0,1,axis=1) # adding the bias
+Y_array = data[:, 0] # Snag the first column corresponding to the letter
+# Y = np.zeros([Y_array.shape[0], 26], dtype = int)
+Y = np.full([Y_array.shape[0], 26], 0.1)
+for i in range(Y_array.shape[0]):
+    Y[i,Y_array[i] - 1] = 0.9
 
-nn = ann_simple.ANN()
-result = nn.foward_learning(X[0]) # 1 row at a time
+# Normalizing each feature
+for i in range(1, 17):
+    X[:,i] = (X[:,i] - X[:,i].min()) / (float(X[:,i].max()) - X[:,i].min())
 
-print(result)
+nn = ann.ANN()
+for i in range(0, 500):
+    yhat = nn.foward(X[i]) # 1 row at a time
+    if np.max(Y[i] - yhat) is not 0.0:
+        nn.back_propagation(X[i], Y[i])
+        numberWrong += 1
 
-
+print(yhat)
+print(numberWrong)
