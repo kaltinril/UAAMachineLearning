Fix x and y input array dimensions. Fix sigmoid_prime calculation. Fix back_prop delta2 to use Z2 instead of SM

Project1/ann.py
@@ -33,7 +33,7 @@ class ANN:
         return 1 / (1 + np.exp(-s))  # 1 / (1 + e^-s)
 
     def sigmoid_prime(self, z):
-        return np.exp(-z) / (1+np.exp(-z)**2)
+        return z*(1-z)  # np.exp(-z) / (1+np.exp(-z)**2)
 
     def softmax(self, z):
         soft = np.full(self.outputLayerSize, 0.10)
@@ -49,13 +49,13 @@ class ANN:
         # print(self.SM)
         # print("SM:",self.SM.shape,"Y:", Y.shape, "S2:", self.S2.shape)
         # print(np.subtract(Y,self.SM))
-        delta2 = np.multiply(-(np.subtract(Y,self.SM)), self.sigmoid_prime(self.S2))
-        print("D2",delta2)
+        delta2 = np.multiply(-(np.subtract(Y, self.Z2)), self.sigmoid_prime(self.S2))
+        #print("D2",delta2)
         djdW2 = np.dot(self.Z1.T, delta2)
 
-        delta = np.dot(delta2, self.W2.T)*self.sigmoid_prime(self.S1)
+        delta = np.dot(delta2, self.W2.T)*self.sigmoid_prime(self.Z1)
         djdW1 = np.dot(X.T, delta)
-        self.W2 = self.W2 - self.learn*djdW2
-        self.W1 = self.W1 - self.learn*djdW1
+        self.W2 = self.W2 + self.learn*djdW2
+        self.W1 = self.W1 + self.learn*djdW1
 
 

Project1/letter_ocr.py
@@ -23,13 +23,23 @@ for i in range(1, 17):
     X[:,i] = (X[:,i] - X[:,i].min()) / (float(X[:,i].max()) - X[:,i].min())
 
 nn = ann.ANN()
-for i in range(0, 500):
+batch_size = 500
+for i in range(0, batch_size):
+    # Fix the issue with Numpy array being (17,) instead of (1,17)
     x = X[i].reshape(X[i].shape[0], 1).T
+    # print("X Shape",x.shape)
     y = Y[i].reshape(Y[i].shape[0], 1).T
     yhat = nn.foward(x) # 1 row at a time
-    if np.max(y - yhat) is not 0.0:
+    y_letter = np.argmax(y)
+    yhat_letter = np.argmax(yhat)
+    print(y_letter, yhat_letter)
+    if y_letter != yhat_letter:
         nn.back_propagation(x, y)
         numberWrong += 1
 
 print(yhat)
-print(numberWrong)
+print("Batch size:", batch_size)
+print("Number Wrong:",numberWrong)
+percent_wrong = (numberWrong / batch_size) * 100
+print("Error percent:",percent_wrong,"%")
+print("Accuracy percent:", (100 - percent_wrong),"%")