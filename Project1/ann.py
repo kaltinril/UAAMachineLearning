Trying to fix my code to use the correct softmax, removing bias from delta calulcations, etc

Project1/ann.py
@@ -4,13 +4,13 @@ class ANN:
     def __init__(self):
         # Layer definitions
         self.inputLayerSize = 17
-        self.hiddenLayerSize = 17
+        self.hiddenLayerSize = 16
         self.outputLayerSize = 26
-        self.learn = 1
+        self.learn = 0.01
 
         # Layer arrays
         self.W1 = np.random.random_integers(-1, 1, (self.inputLayerSize, self.hiddenLayerSize))
-        self.W2 = np.random.random_integers(-1, 1, (self.hiddenLayerSize, self.outputLayerSize))
+        self.W2 = np.random.random_integers(-1, 1, (self.hiddenLayerSize+1, self.outputLayerSize))
         self.S1 = None
         self.S2 = None
         self.Z1 = None
@@ -21,13 +21,23 @@ class ANN:
         # input to Layer1
         self.S1 = np.dot(X, self.W1)
         self.Z1 = self.sigmoid(self.S1)
+
+        # Add in a bias column
+        self.Z1 = np.append(self.Z1, np.ones((self.Z1.shape[0], 1)), axis=1)
+
         self.S2 = np.dot(self.Z1, self.W2)
-        self.Z2 = self.sigmoid(self.S2) #self.Z2 = yhat
-        self.SM = self.softmax(self.Z2)
+        #self.Z2 = self.sigmoid(self.S2) #self.Z2 = yhat
+        self.Z2 = self.f_softmax(self.S2)
+
+        #soft = self.f_softmax(self.S2)
+        #print(soft)
+        #print(np.sum(soft))
+        #self.SM = self.softmax(self.Z2)
+
         #print("Z1", self.Z1.shape)
-        # print("W2", self.W2.shape)
-        # print("S2", self.S2.shape)
-        return self.SM
+        #print("W2", self.W2.shape)
+        #print("S2", self.S2.shape)
+        return self.Z2
 
     def sigmoid(self, s):
         return 1 / (1 + np.exp(-s))  # 1 / (1 + e^-s)
@@ -36,25 +46,42 @@ class ANN:
         return z*(1-z)  # np.exp(-z) / (1+np.exp(-z)**2)
 
     def softmax(self, z):
-        soft = np.full(self.outputLayerSize, 0.10)
+        soft = np.full(self.outputLayerSize, 0)
         max_index = np.argmax(z)
-        soft[max_index] = 0.9
+        soft[max_index] = 1
         return soft
+
+    def f_softmax(self, X):
+        Z = np.sum(np.exp(X), axis=1)
+        Z = Z.reshape(Z.shape[0], 1)
+        return np.exp(X) / Z
 
     # Back popagating error (Delta)
     # DELTA[n] = -(y-y_hat) * sigmoid_prime(self.Z)
     ## delta[n] = (-Y - self.SM) *
     # where n refers to the layer starting with 0 for input
     def back_propagation(self, X, Y):
-        delta3 = self.Z2 - Y
+        delta3 = (self.Z2 - Y)
         delta2 = np.multiply(delta3, self.sigmoid_prime(self.Z2))
-        delta = np.dot(delta2, self.W2.T) * self.sigmoid_prime(self.Z1)
+
+        # Exclude bias column from delta calculation
+        wnobias = self.W2[0:-1, :]
+        delta = np.dot(wnobias, delta2.T) * self.sigmoid_prime(self.Z1)
 
         # Weight updates
-        djdW1 = np.dot(X.T, delta)
+        djdW1 = np.dot(X, delta.T)
         djdW2 = np.dot(self.Z1.T, delta2)
 
         self.W2 = self.W2 - self.learn*djdW2
         self.W1 = self.W1 - self.learn*djdW1
 
+    def printShapes(self):
+
+        print("W1", self.W1.shape)
+        print("S1", self.S1.shape)
+        print("Z1", self.Z1.shape)
+        print("W2", self.W2.shape)
+        print("S2", self.S2.shape)
+        print("Z2", self.Z2.shape)
+        print("SoftMax", self.SM.shape)
 

Project1/letter_ocr.py
@@ -26,30 +26,36 @@ for i in range(1, 17):
 actual_vs_predicted = np.full((26,26), 0)
 
 nn = ann.ANN()
-iterations = 1000
-batch_size = 160
-numberWrong = 0
-for j in range(0, iterations):
-    for i in range(0, batch_size):
-        # Fix the issue with Numpy array being (17,) instead of (1,17)
-        x = X[i].reshape(X[i].shape[0], 1).T
-        # print("X Shape",x.shape)
-        y = Y[i].reshape(Y[i].shape[0], 1).T
-        yhat = nn.foward(x) # 1 row at a time
-        y_letter = np.argmax(y)
-        yhat_letter = np.argmax(yhat)
-        actual_vs_predicted[y_letter, yhat_letter] += 1
-        #print(y_letter, yhat_letter)
-        if y_letter != yhat_letter:
-            nn.back_propagation(x, y)
-            numberWrong += 1
+epochs = 500
+iterations = 160
+batch_size = 100
+x = 0
+
+for h in range(epochs):
+    for j in range(0, iterations):
+        for i in range(0, batch_size):
+            # Fix the issue with Numpy array being (17,) instead of (1,17)
+            x = X[i].reshape(X[i].shape[0], 1).T
+            # print("X Shape",x.shape)
+            y = Y[i].reshape(Y[i].shape[0], 1).T
+            yhat = nn.foward(x) # 1 row at a time
+            y_letter = np.argmax(y)
+            yhat_letter = np.argmax(yhat)
+            actual_vs_predicted[y_letter, yhat_letter] += 1
+            #print(y_letter, yhat_letter)
+            if y_letter != yhat_letter:
+                nn.back_propagation(x, y)
+                numberWrong += 1
 
 #print(actual_vs_predicted)
 
 print("Batch size:", batch_size)
 print("Number Wrong:",numberWrong)
-total_rows = iterations * batch_size
+total_rows = iterations * batch_size * epochs
 print("Number possible:", total_rows)
 percent_wrong = (numberWrong / total_rows) * 100
 print("Error percent:",percent_wrong,"%")
 print("Accuracy percent:", (100 - percent_wrong),"%")
+
+print("X[i]",x.shape)
+nn.printShapes()