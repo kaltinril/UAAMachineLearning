e3ea4baf9c44ae248c47a92d9ec318f82fc4b685
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-03-27 21:49:27 -0800

    Move data combination to seperate python file.  Basic FFT analysis.

--- Assign4/clustering.py
+++ Assign4/clustering.py
@@ -1,43 +1,20 @@
 import numpy as np
-import pandas as pd
-import time
+import matplotlib.pyplot as plt
 
+final_array = np.load('./data_combined.npy')
 
+col = 3
 
+result = np.fft.fft(final_array[0:126, col:col+1].flatten())
 
-# Loop over all the activities, all the people, and all the samples and load the data.
-print("Building Filename List")
-start = time.clock()
-data = None
-y = None
-fnames = []
-for activity in range(1, 20):
-    for person in range(1, 9):
-        for sample in range(1, 61):
-            fnames.append('./ClusteringData/data/a'
-                          + format(activity, '02d')
-                          + '/p' + str(person) + '/s'
-                          + format(sample, '02d')
-                          + '.txt')
+plt.plot(final_array[0:126, col:col+1], 'v-')
+plt.show()
 
-end = time.clock()
-print("Elapsed Filename Building:", end - start)
-print('')
-print("Loading data")
-start = time.clock()
-arrays = [np.loadtxt(f, delimiter=',') for f in fnames]
-end = time.clock()
-print("Elapsed Loading Time:", end - start)
-print('')
+plt.plot(np.real(result), 'o-')
+plt.show()
 
-print('Concatenating files data into a single array')
-start = time.clock()
-final_array = np.concatenate(arrays)
-end = time.clock()
-print("Elapsed Concatenating Time:", end - start)
-print('')
-
-print(final_array.shape)
+plt.plot(np.imag(result), 'x-')
+plt.show()
 
 fnames = None
 arrays = None
