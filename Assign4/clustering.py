4234422f78c939d4d5857172371afc47190aa5a7
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-03-29 23:01:50 -0800

    Playing with PSD

--- Assign4/clustering.py
+++ Assign4/clustering.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from scipy import signal
 
 
 def data2np(graph_data, format):
@@ -28,12 +29,35 @@ final_array = np.load('./data_combined.npy')
 
 final_fft_array = []
 
+x = np.arange(0, 100, 0.1)
+y = np.sin(x)
+#result = np.fft.fft(y)
+result = np.fft.fft(final_array[0:125, 0:1].flatten())
+#real_result = np.real(result)
+real_result = np.real(result)
+second_result = signal.periodogram(real_result[1:125], 25)
+
+cv2.imshow('orig', data2np(final_array[0:125, 0:1], 'r-'))
+cv2.imshow('fft', data2np(real_result[1:125], 'b-'))
+cv2.imshow('periodgram', data2np(second_result, 'g-'))
+cv2.waitKey()
+
+f, Pxx_den = signal.periodogram(real_result[1:125], 25)
+plt.semilogy(f, Pxx_den)
+plt.ylim([1e-7, 1e2])
+plt.xlabel('frequency [Hz]')
+plt.ylabel('PSD [V**2/Hz]')
+plt.show()
+
+
+
+
 for sample_start in range(0, final_array.shape[0], 125):
     fft_array = []
     for col in range(0, final_array.shape[1]):
 
         result = np.fft.fft(final_array[sample_start:sample_start+125, col:col+1].flatten())
-        fft_array.append(np.real(result).T)
+        fft_array.append(np.real(result[1:125]).T)  # Strip off the first 
 
         #orig = data2np(final_array[0:126, col:col+1], 'r-')
         #real = data2np(np.real(result[1:126]), 'b-')
@@ -64,8 +88,8 @@ final_array = None
 print(len(final_fft_array))
 
 final_fft_array = np.concatenate(final_fft_array)
-
 print(final_fft_array.shape)
+
 
 final_fft_array = None
 
